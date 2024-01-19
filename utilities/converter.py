7c114bd2778e3e7ed494947c6500c17c97c16029
Simpleuser <124284281+simpleuserdontwatch@users.noreply.github.com>
2024-01-19 21:04:04 +0500

    Add files via upload

--- utilities/converter.py
+++ utilities/converter.py
@@ -1,12 +1,12 @@
-from PIL import Image
+"""
+The converter of .ico to .png (Batch)
+"""
 import glob
 import os
+from PIL import Image
 
 for i in glob.glob('*.ico'):
     a = Image.open(i)
-
     a.save(a.filename.split('.')[0]+".png")
-
     a.close()
-    
     os.remove(i)
